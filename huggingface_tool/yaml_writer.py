e430dc1b1e6af7f5ba1fe53a0ab151b31160d579
Mateus Matias <mateus.ribeiro@ccc.ufcg.edu.br>
2024-11-11 16:02:08 -0300

    fix: yaml indent

--- huggingface_tool/yaml_writer.py
+++ huggingface_tool/yaml_writer.py
@@ -2,11 +2,16 @@ import os
 
 
 def write_model_metadata_to_yaml(
-    hf_repo: str, des: str, parameters: str | int, release_date: str, tags: list, output_path="."
+    hf_repo: str,
+    des: str,
+    parameters: str | int,
+    release_date: str,
+    tags: list,
+    output_path=".",
 ) -> None:
     """
-    Write the model metadata to a yaml file 
-    
+    Write the model metadata to a yaml file
+
     Args:
     hf_repo (str): The Hugging Face repository name
     des (str): The description of the model
@@ -15,18 +20,18 @@ def write_model_metadata_to_yaml(
     tags (list): The tags for the model metadata
     output_path (str): The output path to write the yaml file
     """
-    
+
     org, title = hf_repo.split("/")
 
     model_metadata = f"""
-    - name: {hf_repo}
-    display_name: {title.replace("-", " ").title()}
-    description: {des}
-    creator_organization_name: {org.upper()}
-    access: open
-    num_parameters: {parameters}
-    release_date: {release_date}
-    tags: {[tag for tag in tags]}
+    -   name: {hf_repo}
+        display_name: {title.replace("-", " ").title()}
+        description: {des}
+        creator_organization_name: {org.upper()}
+        access: open
+        num_parameters: {parameters}
+        release_date: {release_date}
+        tags: {[tag for tag in tags]}
 """
 
     metadata_file_path = os.path.join(output_path, "model_metadata.yaml")
@@ -46,27 +51,29 @@ def write_model_metadata_to_yaml(
         f.write(content)
 
 
-def write_model_deployment_to_yaml(hf_repo: str, max_seq_length: int, output_path=".") -> None:
+def write_model_deployment_to_yaml(
+    hf_repo: str, max_seq_length: int, output_path="."
+) -> None:
     """
     Write the model deployment to a yaml file
-    
+
     Args:
     hf_repo (str): The Hugging Face repository name
     max_seq_length (int): The maximum sequence length of the model
     output_path (str): The output path to write the yaml file
     """
-    
+
     title = hf_repo.split("/")[-1]
 
     model_deployment = f"""
-    - name: huggingface/{title}
-    model_name: {hf_repo}
-    tokenizer_name: {hf_repo}
-    max_sequence_length: {max_seq_length}
-    client_spec:
-        class_name: "helm.clients.huggingface_client.HuggingFaceClient"
-        args:
-        pretrained_model_name_or_path: {hf_repo}
+    -   name: huggingface/{title}
+        model_name: {hf_repo}
+        tokenizer_name: {hf_repo}
+        max_sequence_length: {max_seq_length}
+        client_spec:
+            class_name: "helm.clients.huggingface_client.HuggingFaceClient"
+            args:
+            pretrained_model_name_or_path: {hf_repo}
 """
 
     deployment_file_path = os.path.join(output_path, "model_deployments.yaml")
@@ -84,19 +91,22 @@ def write_model_deployment_to_yaml(hf_repo: str, max_seq_length: int, output_pat
 
     with open(deployment_file_path, "w") as f:
         f.write(content)
-        
-def write_tokenizer_configs_to_yaml(hf_repo: str, prefix_token="", end_of_text_token="", output_path=".") -> None:
+
+
+def write_tokenizer_configs_to_yaml(
+    hf_repo: str, prefix_token="", end_of_text_token="", output_path="."
+) -> None:
     """
-        Write the tokenizer configs to a yaml file
-        
-        Args:
-        hf_repo (str): The Hugging Face repository name
-        prefix_token (str): The prefix token for the tokenizer
-        end_of_text_token (str): The end of text token for the tokenizer
-        output_path (str): The output path to write the yaml file
+    Write the tokenizer configs to a yaml file
+
+    Args:
+    hf_repo (str): The Hugging Face repository name
+    prefix_token (str): The prefix token for the tokenizer
+    end_of_text_token (str): The end of text token for the tokenizer
+    output_path (str): The output path to write the yaml file
     """
     tokenizer_configs = f"""
-    - name: {hf_repo}
+    -   name: {hf_repo}
         tokenizer_spec:
         class_name: "helm.tokenizers.huggingface_tokenizer.HuggingFaceTokenizer"
         args:
@@ -104,21 +114,20 @@ def write_tokenizer_configs_to_yaml(hf_repo: str, prefix_token="", end_of_text_t
         end_of_text_token: "{end_of_text_token}"
         prefix_token: "{prefix_token}"
     """
-    
+
     # Define the output file path
     tokenizer_file_path = os.path.join(output_path, "tokenizer_configs.yaml")
-    
+
     try:
         with open(tokenizer_file_path, "r") as f:
             content = f.read()
     except FileNotFoundError:
         content = "tokenizer_configs:\n"
-        
+
     if not content.startswith("tokenizer_configs:"):
         content = "tokenizer_configs:\n" + content
-        
+
     content += tokenizer_configs
-    
+
     with open(tokenizer_file_path, "w") as f:
         f.write(content)
-        
